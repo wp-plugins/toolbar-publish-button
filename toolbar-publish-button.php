b79de50e5b3e50503c84924e4d2682bff8100cc0
webbistro <webbistro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-29 22:38:03 +0000

    v1.0.4
    git-svn-id: https://plugins.svn.wordpress.org/toolbar-publish-button/tags/1.0.3@689047 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- toolbar-publish-button.php
+++ toolbar-publish-button.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Toolbar Publish Button
 Plugin URI: http://wordpressuxsolutions.com
-Description: Get a copy of Update / Publish / Submit for Review / Save Changes button to the top Toolbar! There is no longer need to scroll an admin pages up and down to edit and save any type of your posts, taxonomies, users or settings.
+Description: Get a copy of Update / Publish / Submit for Review / Save Changes button to the top Toolbar! There is no longer need to scroll admin pages up and down to edit and save any type of your posts, taxonomies, users or settings.
 Version: 1.0.3
 Author: WordPress UX Solutions
 Author URI: http://wordpressuxsolutions.com
@@ -18,7 +18,9 @@ function wpuxss_tpb_admin_scripts()
 		wp_enqueue_script(
 			'toolbar-publish-button',
 			plugins_url( '/tpb.js' , __FILE__ ),
-			array('jquery')
+			array('jquery'),
+			null,
+			true
 		);
 	}
 }
