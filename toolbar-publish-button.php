40381e391b981e73a2deb2fb8fa809c7a6210df5
webbistro <webbistro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-18 21:49:28 +0000

    1.1.2 acticvation process improved
    git-svn-id: https://plugins.svn.wordpress.org/toolbar-publish-button/trunk@742931 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- toolbar-publish-button.php
+++ toolbar-publish-button.php
@@ -34,8 +34,8 @@ $wpuxss_tpb_old_version = get_option('wpuxss_tpb_version', false);
 /**
  *  Load plugin text domain
  *
- *  @since	1.1.0
- *  @created	15/07/13
+ *  @since    1.1.0
+ *  @created  15/07/13
  */
 
 load_plugin_textdomain('toolbar-publish-button', false, basename( dirname( __FILE__ ) ) . '/languages' );
@@ -44,8 +44,8 @@ load_plugin_textdomain('toolbar-publish-button', false, basename( dirname( __FIL
 /**
  *  wpuxss_tpb_admin_scripts
  *
- *  @since	1.0.2
- *  @created	30/03/13
+ *  @since    1.0.2
+ *  @created  30/03/13
  */
  
 add_action( 'admin_init', 'wpuxss_tpb_admin_scripts' );
@@ -130,8 +130,8 @@ function wpuxss_tpb_admin_scripts()
  *
  *  Add plugin settings page
  *
- *  @since	1.1.0
- *  @created	15/07/13
+ *  @since    1.1.0
+ *  @created  15/07/13
  */
 
 add_action('admin_menu', 'wpuxss_tpb_admin_menu');
@@ -152,9 +152,9 @@ function wpuxss_tpb_admin_menu()
  *
  *  Print plugin options page
  *
- *  @type	callback function
- *  @since	1.1.0
- *  @created	15/07/13
+ *  @type     callback function
+ *  @since    1.1.0
+ *  @created  15/07/13
  */
 
 function wpuxss_tpb_print_options() 
@@ -163,73 +163,79 @@ function wpuxss_tpb_print_options()
 	?>  
     
 	<div id="toolbar-publish-button-settings-wrap" class="wrap">    
-    	<?php screen_icon(); ?>
-        <h2><?php _e('Toolbar Publish Button Settings','toolbar-publish-button'); ?></h2>   
+		<?php screen_icon(); ?>
+		<h2><?php _e('Toolbar Publish Button Settings','toolbar-publish-button'); ?></h2>   
         
-        <div id="poststuff">
-        	<div id="post-body" class="metabox-holder columns-2">
+		<div id="poststuff">
+		
+			<div id="post-body" class="metabox-holder columns-2">
 
-                <div id="postbox-container-2" class="postbox-container">
-                	<div class="postbox">
-                        <form method="post" action="options.php">
-                            <?php settings_fields( 'wpuxss_tpb_settings' ); ?>
+				<div id="postbox-container-2" class="postbox-container">
+				
+					<div class="postbox">
+						<form method="post" action="options.php">
+
+							<?php settings_fields( 'wpuxss_tpb_settings' ); ?>
                             
-                            <h3><?php _e('Basic Settings','toolbar-publish-button'); ?></h3>
+							<h3><?php _e('Basic Settings','toolbar-publish-button'); ?></h3>
                             
-                            <div class="inside">
-                            
-                                <ul class="wpuxss_tpb_settings_list">
+							<div class="inside">
+                           
+								<ul class="wpuxss_tpb_settings_list">
                                 
-                                    <li>
-                                        <?php $options = get_option('wpuxss_tpb_settings'); ?>
-                                        <input id="wpuxss_tpb_scrollbar_return" name="wpuxss_tpb_settings[wpuxss_tpb_scrollbar_return]" type="checkbox" value="1" <?php checked( '1', $options['wpuxss_tpb_scrollbar_return'] ); ?> />
-                                        <label>
-                                            <?php _e('Remember scrollbar position','toolbar-publish-button'); ?><br />
-                                            <span><?php _e('It will return admin page scroll bar to the position that preceded the click of the Save/Update button','toolbar-publish-button'); ?></span>
-                                        </label>
-                                    </li>
+									<li>
+										<?php $options = get_option('wpuxss_tpb_settings'); ?>
+										<input id="wpuxss_tpb_scrollbar_return" name="wpuxss_tpb_settings[wpuxss_tpb_scrollbar_return]" type="checkbox" value="1" <?php checked( '1', $options['wpuxss_tpb_scrollbar_return'] ); ?> />
+										<label>
+											<?php _e('Remember scrollbar position','toolbar-publish-button'); ?><br />
+											<span><?php _e('It will return admin page scroll bar to the position that preceded the click of the Save/Update button','toolbar-publish-button'); ?></span>
+										</label>
+									</li>
                                     
-                                </ul>
+								</ul>
                                 
-                                <?php submit_button(); ?>
+								<?php submit_button(); ?>
                             
-                            </div>
+							</div>
 
-                        </form>
-                    </div>
-                </div>
+						</form>
+					</div>
+					
+				</div>
                 
-                <div id="postbox-container-1" class="postbox-container">
-                    <div class="postbox">
+				<div id="postbox-container-1" class="postbox-container">
+				
+					<div class="postbox">
                         
-                        <h3>Toolbar Publish Button <?php echo $wpuxss_tpb_version; ?></h3>
+						<h3>Toolbar Publish Button <?php echo $wpuxss_tpb_version; ?></h3>
                         
-                        <div class="inside">
+						<div class="inside">
                         
-                            <h4>Changelog</h4>
-                            <p>What's new in <a href="http://wordpress.org/plugins/toolbar-publish-button/changelog/">version <?php echo $wpuxss_tpb_version; ?></a>.</p>
+							<h4>Changelog</h4>
+							<p>What's new in <a href="http://wordpress.org/plugins/toolbar-publish-button/changelog/">version <?php echo $wpuxss_tpb_version; ?></a>.</p>
+							
+							<h4>Support</h4>
+							<p>Feel free to ask for support on the <a href="http://wordpress.org/support/plugin/toolbar-publish-button/">wordpress.org</a> or on the <a href="http://wordpressuxsolutions.com/support/">plugin website</a>.</p>
+							
+							<h4>Plugin raiting</h4>
+							<p>Please <a href="http://wordpress.org/support/view/plugin-reviews/toolbar-publish-button/">vote for the plugin</a>. Thanks!</p>
                             
-                            <h4>Support</h4>
-                            <p>Feel free to ask for support on the <a href="http://wordpress.org/support/plugin/toolbar-publish-button/">wordpress.org</a> or on the <a href="http://wordpressuxsolutions.com/support/">plugin website</a>.</p>
-                            
-                            <h4>Plugin raiting</h4>
-                            <p>Please <a href="http://wordpress.org/support/view/plugin-reviews/toolbar-publish-button/">vote for the plugin</a>. Thanks!</p>
-                            
-                            <div class="author">
-                                <span><a href="http://wordpressuxsolutions.com/">WordPress UX Solutions</a> by <a class="logo-webbistro" href="http://twitter.com/webbistro"><span class="icon-webbistro">@</span>webbistro</a></span>
-                            </div>
+							<div class="author">
+								<span><a href="http://wordpressuxsolutions.com/">WordPress UX Solutions</a> by <a class="logo-webbistro" href="http://twitter.com/webbistro"><span class="icon-webbistro">@</span>webbistro</a></span>
+							</div>
                         
-                        </div>
+						</div>
     
-                    </div>
-                </div>
+					</div>
+					
+				</div>
             
-        	</div>
-        </div>
+			</div>
+			
+		</div>
+		
 	</div>
-    
-    
-    
+ 
 	<?php
 }
 
@@ -237,9 +243,9 @@ function wpuxss_tpb_print_options()
 /**
  *  wpuxss_tpb_settings_validate
  *
- *  @type	callback function
- *  @since	1.1.0
- *  @created	15/07/13
+ *  @type     callback function
+ *  @since    1.1.0
+ *  @created  15/07/13
  */ 
  
 function wpuxss_tpb_settings_validate($input)
@@ -258,11 +264,11 @@ function wpuxss_tpb_settings_validate($input)
  *
  *  Set default value for plugin setting during plugin activation
  *
- *  @since	1.1.0
- *  @created	15/07/13
+ *  @since    1.1.0
+ *  @created  15/07/13
  */ 
 
-register_activation_hook(  __FILE__, 'wpuxss_tpb_on_activation' );
+//register_activation_hook(  __FILE__, 'wpuxss_tpb_on_activation' );
 function wpuxss_tpb_on_activation() 
 {
 	$wpuxss_tpb_settings = array(
@@ -277,8 +283,8 @@ function wpuxss_tpb_on_activation()
  *
  *  Add settings link to the plugin action links
  *
- *  @since	1.1.0
- *  @created	15/07/13
+ *  @since    1.1.0
+ *  @created  15/07/13
  */ 
  
 add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'wpuxss_tpb_settings_links' );
