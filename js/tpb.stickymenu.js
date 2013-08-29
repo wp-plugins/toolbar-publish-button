8668d3cdc8dc95506118adbf6d9c177df56ad449
webbistro <webbistro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-29 17:21:54 +0000

    tag 1.2.3
    git-svn-id: https://plugins.svn.wordpress.org/toolbar-publish-button/tags/1.2.3@764342 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/tpb.stickymenu.js
@@ -0,0 +1,100 @@
+(function($) {	
+	
+	function saveStickyMenuCookie ()
+	{
+		$.cookie( 'TPBmenuScrollTop', window.menuScrollTop );
+	}
+	
+	function removeCookie ()
+	{			
+		$.removeCookie( 'TPBmenuScrollTop' );
+	}
+	
+	$(function(ready)
+	{		
+		var button = $('input[type="submit"].button-primary, input[type="button"].button-primary, input[type="submit"].acf-button');
+		
+		button.add('.row-actions-visible .activate a, .row-actions-visible .deactivate a').click(function(e) 
+		{
+			saveStickyMenuCookie();
+		});
+
+		$('#adminmenuwrap, #adminmenuback').wrapAll('<div id="adminmenusuperwrap" />');
+			
+		window.lastScrollTop = 0;
+		window.menuScrollTop = 0;
+		window.prevMenuScrollTop = $.cookie( 'TPBmenuScrollTop' ) || 0;
+		
+		if ( $('body.mp6').length )
+				window.menuWidth = parseInt($('#adminmenuwrap').width());
+			else 
+				window.menuWidth = parseInt($('#adminmenuwrap').width()) + 10;
+			
+		$('#adminmenusuperwrap').css({
+			'position' : 'fixed',
+			'z-index' : 20,
+			'top' : $('#wpadminbar').height(),
+			'height' : $(window).height() - $('#wpadminbar').height(),
+			'width' : window.menuWidth,
+			'overflow' : 'hidden'
+		});
+		
+		$('#wpcontent').css({'z-index' : 10});
+		$('#wpadminbar').prependTo('#wpwrap');
+		
+		$(document).on("mouseover", "#adminmenuwrap", function(e)
+		{				
+			$('#adminmenusuperwrap').width( window.menuWidth + parseInt($('.wp-submenu.wp-submenu-wrap').width()) + 10 );
+		});
+	
+		$(document).on("mouseleave", "#adminmenuwrap", function(e)
+		{
+			$('#adminmenusuperwrap').width( window.menuWidth );
+		});
+		
+		$('#collapse-menu').click(function(e)
+		{
+			if ( $('body.mp6').length )
+				window.menuWidth = parseInt($('#adminmenuwrap').width());
+			else 
+				window.menuWidth = parseInt($('#adminmenuwrap').width()) + 10;
+				
+			$('#adminmenusuperwrap').width( window.menuWidth );
+		});
+	});
+	
+	$(window).on('load',function(e)
+	{
+		if ( $('#adminmenuwrap').height() > $(document).height() )
+			$('#wpcontent').height($('#adminmenuwrap').height());
+
+		removeCookie();
+	});
+
+	$(window).on('resize',function(e)
+	{
+		$('#adminmenusuperwrap').css({
+			'height' : $(this).height() - $('#wpadminbar').height()
+		});
+		
+		if ( $('#adminmenuwrap').height() > $(document).height() )
+			$('#wpcontent').height($('#adminmenuwrap').height());
+	});
+	
+	$(window).on('scroll',function(e)
+	{
+		if ( window.prevMenuScrollTop )
+		{
+			$('#adminmenusuperwrap').scrollTop( window.prevMenuScrollTop );
+			window.prevMenuScrollTop = 0;
+		}
+		else
+			$('#adminmenusuperwrap').scrollTop( window.menuScrollTop + $(this).scrollTop() - window.lastScrollTop );
+		
+		window.menuScrollTop = $('#adminmenusuperwrap').scrollTop();			
+		window.lastScrollTop = $(this).scrollTop();
+		
+		saveStickyMenuCookie();
+	});
+
+})( jQuery );
